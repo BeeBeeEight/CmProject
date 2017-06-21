2876ea52de471f3524c07d8aff050584d6d7a29d
Teun Roelandt <teun977@gmail.com>
2017-06-21 15:54:26 +0200

    alle links

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
-
 var express = require('express');
 var router = express.Router();
 var path = require('path');
 var request = require('request');
 var EventEmitter = require("events").EventEmitter;
 var body = new EventEmitter();
-var main = require('../server');
+var token;
+var groupid;
+var amount;
+//var phonenumber = NULL; //Wel nodig voor Alexa
 
 var auth = {
     url : 'https://autocollectapi.cmpayments.com/v1.0/token',
@@ -17,18 +19,112 @@ var auth = {
     body: 'grant_type=password&username=Avans1ApiUser&password=59bf8b536a0802561c8be4e3fd1b300847f5549d190499670921a3e40467d707'
 };
 
-var authrequest = request(auth, function(err, res, data) {  
-//    body.data = data;    
-//    body.emit('update');
+
+
+request(auth, function(err, res, data) {  
     var result = JSON.parse(data);
-    var token = result.access_token;
-    main.token = token;
+    token = result.access_token;
+    console.log(token);
+
+                var payment_plan_get = {
+                    url : 'https://autocollectapi.cmpayments.com/v1.0/payment-plans',
+                    method: 'GET',
+                    headers: {
+                        'Content-Type':'application/json',
+                        'Authorization':'Bearer '+ token + '' 
+                    }  
+                };
+
+                var group_get = {
+                    url : 'https://autocollectapi.cmpayments.com/v1.0/groups',
+                    method: 'GET',
+                    headers: {
+                        'Content-Type':'application/json',
+                        'Authorization':'Bearer ' + token + ''
+                    }
+                };
+
+                          
+
+
+
+
+                request(payment_plan_get, function(err, res, body) {
+                    var result = JSON.parse(body);
+                    console.log(result);
+
+                        request(group_get, function(err, res, body) {
+                        var result = JSON.parse(body);
+                        groupid = result[0].group_id;
+                        console.log(result);
+                        console.log("groupid: " + groupid);
+
+
+
+                        var debtor_get = {
+                            url: 'https://autocollectapi.cmpayments.com/v1.0/groups/' + groupid + '/debtors',
+                            method: 'GET',
+                            headers: {
+                            'Content-Type':'application/json',
+                            'Authorization':'Bearer ' + token + ''
+                            }
+                        }
+    
+                        var check_out_get = {
+                            url: 'https://autocollectapi.cmpayments.com/v1.0/groups/' + groupid + '/check-outs', 
+                            method: 'GET',
+                            headers: {
+                                'Content-type':'application/json',
+                                'Authorization':'Bearer ' + token + ''
+                            }
+                        }  
+
+                            request(debtor_get, function(err, res, body) {
+                            var result = JSON.parse(body);
+                            console.log(result);
+
+
+                                request(check_out_get, function(err, res, body){
+                                var result = JSON.parse(body);
+                                console.log(result);
+
+
+                                    amount = 13.37; //Deze moet de waarde hebben die meegegeven wordt foor alexa/facebook
+
+                                    var debtor_post = {
+                                        url: 'https://autocollectapi.cmpayments.com/v1.0/groups/' + groupid + '/debtors',
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-type':'application/json',
+                                            'Authorization':'Bearer ' + token + ''
+                                        },
+                                        body: '[{"reference": "' + "OWN-DEBTOR-REF" + '", "currency": "EUR", "total_amount": ' + amount + ', "locale": "nl-NL", "no_direct_debit": false}]'
+                                    }
+ 
+                                    var checkout_post = { 
+                                        url: 'https://autocollectapi.cmpayments.com/v1.0/groups/' + groupid + '/check-outs', 
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type':'application/json',
+                                            'Authorization':'Bearer ' + token + ''
+                                        },
+                                        body: '{"reference": "OWN-DEBTOR-REF", "total_amount":' + amount + ', "currency": "EUR", "locale": "nl-NL"}' //verander reference en total amount naar een var
+                                    }
+
+                                        request(debtor_post, function(err, res, body){
+                                            var result = JSON.parse(body);
+                                            console.log(result);
+                                        
+
+                                            request(checkout_post, function(err, res, body){
+                                                var result = JSON.parse(body);
+                                                console.log(result);
+                                            });
+                                        });
+                                }); 
+                            });
+                        });
+                });
 });
-/*
-body.on('update', function (response) {
-    var result = JSON.parse(body.data);
-    var token = result.access_token;
-    main.token = token;
-});
-*/
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-var request = require('request');
-var access_token;
+var config = require('./config.json');
+var express = require('express');
+var app = express();
+var token;
+
+app.set('PORT', config.webPort);
+
+app.all('*', function(request, response, next) {
+ console.log(request.method + " " + request.url);
+ next();
+})
+
+app.use('/auth', require('./routes/auth'));
 
 
-var auth = {
-    url : 'https://autocollectapi.cmpayments.com/v1.0/token',
-    method: 'POST',
-    headers: {
-        'Content-Type':'application/x-www-form-urlencoded',
-        'X-CM-MERCHANT':'AMR-745B5C98-648A-45A1-ACB4-BDAD2AB9E936'
-    },
-    body: 'grant_type=password&username=Avans1ApiUser&password=59bf8b536a0802561c8be4e3fd1b300847f5549d190499670921a3e40467d707'
-};
 
-var paymentplans = {
-    url: 'https://autocollectapi.cmpayments.com/v1.0/payment-plans',
-    method: 'GET',
-    headers: {
-        'Authorisation': access_token, 
-        'Content-Type': 'application/json'
-    }
-};
+app.all('*', function(request, response) {
+ response.status(404);
+ response.send('404 - Not found');
+})
 
-request(auth, function(err, res, body) {  
-    var json = JSON.parse(body);
-    //console.log(json);
-    access_token = json.access_token;
-    console.log(access_token);
-});
-
-
-request(paymentplans, function(err, res, body){
-    var json = JSON.parse(body);
-    console.log(json);
-});
+var port = process.env.PORT || app.get('PORT');
+app.listen(port, function() {
+ console.log('Server app is listening on port' + port);
+})
